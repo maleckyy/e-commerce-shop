5849d7a0f46f1684caf9908db7bbff341798e09c
zexcv <krusher13@o2.pl>
2023-12-01 19:27:19 +0100

    fix

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <div class="img-div">
+      <img :src="item.image" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["item"],
+};
+</script>
+
+<style scoped></style>
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -4,38 +4,8 @@
     <div class="sub-title">Check out our latest products!</div>
 
     <div class="items-wrapper">
-      <!-- lista itemków -->
-      <!-- 
-       -->
-
-      <!-- 
-
-        <compionent v-for="item in/of items" :item="item"></compionent>
-            {
-        "id": 2,
-        "title": "Mens Casual Premium Slim Fit T-Shirts ",
-        "price": 22.3,
-        "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-        "category": "men's clothing",
-        "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-        "rating": {
-            "rate": 4.1,
-            "count": 259
-        }
-        V
-        W komponencie
-        props:['item']
-        :src=""
-
-        div wraper @click=navigteDESC
-
-        
-
-PAMIETAMY O THIS.ITEM
-
-
-
-        -->
+    
+ 
     </div>
   </div>
 </template>
