33b3b9d98bf0bfd78cec794e079288f7a907e066
maleckyy <kupferpatryk@gmail.com>
2023-12-01 17:56:14 +0100

    add store

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <p>hp</p>
+  <a></a>
   <router-view></router-view>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    items() {
+      return this.$store.getters.getItems;
+    },
+  },
+  created() {
+    this.$store.dispatch("getItemsFromAPI");
+  },
+  mounted() {
+    console.log(this.items);
+  },
+};
 </script>
 
 <style scoped>
