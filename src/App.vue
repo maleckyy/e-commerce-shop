cef95cadf64d9b8d7d8fd13d4bacecf8b2279b17
zexcv <krusher13@o2.pl>
2023-12-01 19:06:30 +0100

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
+  <navbar></navbar>
   <p>hp</p>
   <router-view></router-view>
 </template>
 
 <script>
-export default {};
+import NavbarComp from './components/NavbarComp.vue'
+export default {
+  components: {
+    navbar: NavbarComp
+  }
+};
 </script>
 
-<style scoped>
+<style>
 @import "./style.css";
 p {
   color: var(--color-blue);
---
+++ src/components/NavbarComp.vue
@@ -0,0 +1,59 @@
+<template>
+  <navbar class="navbar">
+    <div class="logo">LOGO</div>
+    <ul>
+      <li><a href="">Home</a></li>
+      <li><a href="">Shop</a></li>
+      <li><a href="">Koszyk</a></li>
+    </ul>
+  </navbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.navbar {
+  margin: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  background: var(--color-blue);
+  max-width: 100%;
+}
+ul {
+  gap: 2rem;
+  display: flex;
+  align-items: center;
+  list-style: none;
+}
+li {
+  font-family: var(--font-mont);
+  font-weight: 600;
+}
+a:hover {
+  color: #216d68;
+  text-decoration: underline;
+}
+a {
+  text-decoration: none;
+  color: var(--color-black);
+}
+div {
+  color: red;
+}
+
+@media screen and (max-width: 400px) {
+    .navbar{
+        padding: 0.5rem 0.7rem;
+    }
+    ul {
+        gap: 1.5rem;
+    }
+    li {
+        font-size: 0.8rem;
+    }
+}
+</style>
